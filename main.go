788199490d343a86c75fc73e02596ef3ffe423ee
vodkaesc <tharun30115@gmail.com>
2026-03-04 21:28:57 +0530

    added http

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
 	loadReasons()
-	fmt.Printf("first reason: %s\n", reasons[0])
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "no-as-a-service is running")
+	})
+
+	log.Printf("server starting on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
