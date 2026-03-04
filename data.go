b1e934a82c2af72e2c6ae8aad6c345952aa3422c
vodkaesc <tharun30115@gmail.com>
2026-03-04 16:50:38 +0530

    embeded reasons

---
+++ data.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"log"
+)
+
+//embeds reasons.json (go:embed)
+var reasonsFile []byte
+
+var reasons []string
+
+func loadReasons() {
+	err := json.Unmarshal(reasonsFile, &reasons)
+	if err != nil {
+		log.Fatalf("failed to load reasons: %v", err)
+	}
+	log.Printf("loaded %d reasons", len(reasons))
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
+	loadReasons()
+	fmt.Printf("first reason: %s\n", reasons[0])
 }
